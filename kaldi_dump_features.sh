075bea1d8f7d7d80dda8b1d7948fc1e66009b0c2
Rafael Valle <jrafaelvalle@gmail.com>
2016-09-26 23:52:02 -0700

    remove some files

--- kaldi_dump_features.sh
+++
@@ -1,2 +0,0 @@
-~/Desktop/kaldi/src/online2bin/online2-wav-dump-features --ivector-extraction-config=nnet_a_gpu_online/conf/ivector_extractor.conf --config=nnet_a_gpu_online/conf/online_nnet2_dump.conf --verbose=1 "ark:echo utterance-id1 utterance-id1|" "scp:echo utterance-id1 audio/clinton1_8k.wav|" "ark,t:features/clinton1_8k.ark"
-# online2-wav-dump-features [options] <spk2utt-rspecifier> <wav-rspecifier> <feature-wspecifier>
