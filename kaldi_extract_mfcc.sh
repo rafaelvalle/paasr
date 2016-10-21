32a0808d30a77406cd9203c705126e8e1303e91d
Rafael Valle <jrafaelvalle@gmail.com>
2016-10-20 21:35:04 -0700

    script to extract mfcc from audio

---
+++ kaldi_extract_mfcc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+filepath=$1
+filename=$(basename "$1")
+filedir=$(dirname "$1")
+extension="${filename##*.}"
+filename="${filename%.*}"
+curpath=${PWD}
+
+echo "executing $0 with $1"
+~/Desktop/kaldi/src/featbin/compute-mfcc-feats \
+    --config=/Users/rafaelvalle/Desktop/pasr/conf/mfcc.conf \
+    --verbose=1 \
+    "scp:echo $filename $filepath|" \
+    "ark,t:$filedir/$filename.mfcc"
+echo "done with $0 and $1"    
+# compute-mfcc-feats  $vtln_opts --verbose=2 --config=$mfcc_config \
+#      scp,p:$logdir/wav_${name}.JOB.scp
