5264dd16fb8d665c722551189721ff2843959687
Rafael Valle <jrafaelvalle@gmail.com>
2016-10-23 20:53:28 -0700

    including mean and std

--- parameter_search.py
+++ parameter_search.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python
 
-""" Search for good hyperparameters for classifiction using the manually
-perturbed (missing data) ADULT and VOTES datasets.
-"""
 import os
-import pdb
-import numpy as np
+import cPickle as pkl
 import neural_networks
 import bayesian_parameter_optimization as bpo
 from build_kws_data import get_data
-from params import nnet_params, hyperparameter_space, feats_train_folder, MODEL_NAME
-from params import TRIAL_DIRECTORY, MODEL_DIRECTORY
-from params import target_glob_str, other_glob_str, file_durations_path, threshold
-
-
-def set_trace():
-    from IPython.core.debugger import Pdb
-    import sys
-    Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
+from params import nnet_params, hyperparameter_space
+from params import TRIAL_DIRECTORY, MODEL_DIRECTORY, MODEL_NAME
+from params import target_glob_str, other_glob_str, file_durations_path
+from params import MIN_LENGTH, standard_scaler_path
 
 if __name__ == '__main__':
     print("Executing bayesian hyperparameter optimization")
 
     # Load data and convert it to float 32
-    targets, others = get_data(target_glob_str, other_glob_str, file_durations_path, threshold)
-    
+    targets, others = get_data(
+        target_glob_str, other_glob_str, file_durations_path, MIN_LENGTH)
+
+    # add mean and std to nnet_params
+    ss = pkl.load(open(standard_scaler_path, 'rb'))
+    nnet_params['offset'] = ss.mean_
+    nnet_params['scale'] = ss.scale_
+
     # Run parameter optimization FOREVER
     bpo.parameter_search(targets, others,
                          nnet_params,
